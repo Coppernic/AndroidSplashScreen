6a7c7e05b94d9dbb5665de8ba508ef50d33a2e79
Bastien Paul <bastien.paul@coppernic.fr>
2018-03-13 14:42:57 +0100

    [FEATURE] First commit for Android Splash Screen

---
+++ splash/src/main/java/fr/coppernic/lib/splash/SplashScreen.java
@@ -0,0 +1,11 @@
+package fr.coppernic.lib.splash;
+
+public class SplashScreen extends SplashScreenBase {
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        startTargetActivity();
+    }
+
+}
